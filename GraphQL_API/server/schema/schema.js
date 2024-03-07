c0c1162e3404a8da8a70bffc7633e4bece889c92
Cameron Rosencutter <cameronrosencutter@yahoo.com>
2024-03-07 10:55:33 -0600

    push

--- GraphQL_API/server/schema/schema.js
+++ GraphQL_API/server/schema/schema.js
@@ -30,6 +30,12 @@ const tasks = [
     title: 'Advanced HTML',
     weight: 1,
     description:  'Welcome to the Web Stack specialization. The 3 first projects will give you all basics of the Web development: HTML, CSS and Developer tools. In this project, you will learn how to use HTML tags to structure a web page. No CSS, no styling - don’t worry, the final page will be “ugly” it’s normal, it’s not the purpose of this project. Important note: details are important! lowercase vs uppercase / wrong letter… be careful!',
+  },
+  {
+    id: '3.5',
+    title: 'Bootstrap',
+    weight: 1,
+    description: '’Bootstrap is a free and open-source CSS framework directed at responsive, mobile-first front-end web development. It contains CSS and JavaScript design templates for typography, forms, buttons, navigation, and other interface components.'
   }
 ];
 
