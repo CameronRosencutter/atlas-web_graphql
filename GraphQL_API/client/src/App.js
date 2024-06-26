7ae5de7264d669f637d7bcd57292067c093cd403
Cameron Rosencutter <cameronrosencutter@yahoo.com>
2024-06-25 19:17:12 -0500

    pish

--- GraphQL_API/client/src/App.js
+++ GraphQL_API/client/src/App.js
@@ -1,28 +1,20 @@
-/*import ApolloClient from 'apollo-boost';
-/*
-...
-*/
-// components
-import TaskList from './components/TaskList';
-import AddTask from './components/AddTask';
-import AddProject from './components/AddProject';
-// apollo client setup
-/*
-...
-*/
+import React from 'react';
+import ApolloClient from 'apollo-boost';
+import { ApolloProvider } from 'react-apollo';
+
+// Set up Apollo Client
+const client = new ApolloClient({
+  uri: 'http://localhost:4000/graphql'
+});
+
 function App() {
-  return ( <
-    div id = "main" >
-    <
-    div id = "bg" > < /div> <
-    h1 > Holberton school tasks < /h1> <
-    TaskList / >
-    <
-    AddProject / >
-    <
-    AddTask / >
-    <
-    /div>
+  return (
+    <ApolloProvider client={client}>
+      <div className="App">
+        <h1>My GraphQL App</h1>
+        {/* Add your components here */}
+      </div>
+    </ApolloProvider>
   );
 }
 
